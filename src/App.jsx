df0cbeafe47a564cd511f2200d95c7499e491223
Oloremma-dev <isholaolorunjuwonlo5@gmail.com>
2024-01-18 03:03:25 +0100

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Header from "./componenets/Header/Header";
 import Hero from "./componenets/Hero/Hero";
+import "./App.css"
+import Companies from "./componenets/Companies/Companies";
+import Residencies from "./componenets/Residencies/Residencies";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div> 
+      <Companies />
+      <Residencies />
     </div>
   );
 }
